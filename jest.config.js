79884903bfe3427262d7c10910a03c08a4642c05
Måns Berg <mansberg@MacBook-Pro-som-tillhor-Mans.local>
2025-09-18 14:41:23 +0200

    setiing up test enivornment

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,13 @@ const { createDefaultPreset } = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").Config} **/
-export default {
+/** @type {import("jest").Config} */
+module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
+  moduleFileExtensions: ["ts", "js", "json"],
+  testMatch: ["**/?(*.)+(test).[tj]s"],
 };
